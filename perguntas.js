b82f51d1e39aa22ab1afd79b0115c718944719a9
maria-sophia <bonito.maria@escola.pr.gov.br>
2025-06-03 08:03:28 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Programação',
+    'O que é arduino?',
+    'O Arduino Uno é uma placa de microcontrolador de código aberto,'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da grecia?',
+    'A capital da grecia é atenas'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz crianças em Inglês?',
+    'crianças em ingles é children'
+)
+criaCartao( 
+    'lingua portuguesa'
+    'o que e linguagem argumentativa?'
+    'é aquela que tem como objetivo convencer o interlocutor sobre uma determinada ideia ou ponto de vista'
+)
+criaCartao(
+    'matematica'
+    'quais os principais sinais da matematica basica?'
+    'mais,menos,subitrasao,divsao'
+)
